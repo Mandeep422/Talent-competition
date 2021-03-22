43022686595b695de8bcdaac7b89449644c0fd5c
Mandeep Gill <mandeepkaurgill422@gmail.com>
2021-03-23 12:10:14 +1300

    First commit

---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
@@ -0,0 +1,110 @@
+﻿import React from 'react';
+import Cookies from 'js-cookie';
+import { Popup } from 'semantic-ui-react';
+import { Button, Card, Image } from 'semantic-ui-react'
+import moment from 'moment';
+
+export class JobSummaryCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loadJob: {
+                jobDetails: {
+                    location: []
+                }
+            }
+        }
+        this.selectJob = this.selectJob.bind(this)
+    }
+
+    componentDidMount() {
+        this.selectJob(this.props.id);
+    }
+
+    selectJob(id) {
+        var cookies = Cookies.get('talentAuthToken');
+        var link ='http://localhost:51689/listing/listing/GetJobById'
+        //url: 'http://localhost:51689/listing/listing/closeJob',
+        $.ajax({
+            url: link,
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "GET",
+            contentType: "application/json",
+            dataType: "json",
+            data: { id },
+            success: function (res) {
+                let loadJob = null;
+                if (res.jobData) {
+                    loadJob = res.jobData
+                    this.setState({ loadJob: loadJob })                    
+                }
+            }.bind(this),
+            error: function (res) {
+                console.log(res.status)
+            }
+        })
+    }
+
+    render() {
+    /*jobData: {
+            title: "",
+            description: "",
+            logoUrl: "",
+            summary: "",
+            applicantDetails: {
+                yearsOfExperience: { years: 0, months: 0 },
+                qualifications: [],
+                visaStatus:[]
+            },
+            jobDetails: {
+                categories: { category: "", subCategory: "" },
+                jobType: [],
+                startDate: moment(),
+                salary: { from: 0, to: 0 },
+                location: { country: "", city: ""}
+            }
+        }
+*/
+        let job = this.state.loadJob;       
+        let city = job.jobDetails['location'].city;
+        let country = job.jobDetails['location'].country;
+        let location = city + ", " + country;
+        return(
+            <Card.Group>
+                <Card>
+                    <Card.Content>
+
+                        <Card.Header>{this.state.loadJob.title}</Card.Header>
+                        <Card.Meta>{location}</Card.Meta>
+                        <Card.Description>
+                            {job.summary}
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <div className='ui one buttons'>
+                            <Button color='red'>
+                                Expired
+                            </Button>
+                        </div>
+                        <div className='ui three buttons'>
+                            <Button basic color='blue'>
+                                Close
+                            </Button>
+                            <Button basic color='blue'>
+                                Edit
+                            </Button>
+                            <Button basic color='blue'>
+                                Copy
+                            </Button>
+                        </div>
+                    </Card.Content>
+                </Card>
+                
+            </Card.Group>
+
+        )
+    }
+}
---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
@@ -0,0 +1,183 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Cookies from 'js-cookie';
+import LoggedInBanner from '../../Layout/Banner/LoggedInBanner.jsx';
+import { LoggedInNavigation } from '../../Layout/LoggedInNavigation.jsx';
+import { JobSummaryCard } from './JobSummaryCard.jsx';
+import { BodyWrapper, loaderData } from '../../Layout/BodyWrapper.jsx';
+import { Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment, Table } from 'semantic-ui-react';
+
+export default class ManageJob extends React.Component {
+    constructor(props) {
+        super(props);
+        let loader = loaderData
+        loader.allowedUsers.push("Employer");
+        loader.allowedUsers.push("Recruiter");
+        //console.log(loader)
+        this.state = {
+            loadJobs: [],
+            currentJobs: [],
+            loaderData: loader,
+            activePage: 1,
+            sortBy: {
+                date: "desc"
+            },
+            filter: {
+                showActive: true,
+                showClosed: false,
+                showDraft: true,
+                showExpired: true,
+                showUnexpired: true
+            },
+            totalPages: 1,
+            begin: 0,
+            end: 2,
+        }
+        this.loadData = this.loadData.bind(this);
+        this.init = this.init.bind(this);
+        this.loadNewData = this.loadNewData.bind(this);
+        //your functions go here
+        this.onPaginate = this.onPaginate.bind(this);
+    };
+
+    init() {
+        let loaderData = this.state.loaderData;
+        loaderData.allowedUsers.push("Employer");
+        loaderData.allowedUsers.push("Recruiter");
+        loaderData.isLoading = false;
+        this.setState({ loaderData, })
+    }
+
+    componentDidMount() {
+        this.init();
+        this.loadData();
+
+    };
+
+    loadData(callback) {
+        var link = 'http://localhost:51689/listing/listing/getEmployerJobs';
+        var cookies = Cookies.get('talentAuthToken');
+       // your ajax call and other logic goes here
+        $.ajax({
+            url: link,
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "GET",
+            contentType: "application/json",
+            dataType: "json",
+            success: function (res) {
+                let loadJobs = null;
+                if (res.myJobs) {
+                    loadJobs = res.myJobs
+                }
+           //     let jobList = Object.assign({}, this.state.loadJobs, loadJobs)
+                this.setState(
+                    {
+                        loadJobs: loadJobs
+                    })
+                this.setState({
+                    currentJobs: this.state.loadJobs.slice(this.state.begin, this.state.end)
+                })
+                console.log("all jobs...... begining");
+                console.log(this.state.loadJobs);
+                console.log("current jobs......begining");
+                console.log(this.state.currentJobs);
+            }.bind(this),
+            error: function (res) {
+                console.log(res.Message)
+            }
+            
+        })
+       this.init()
+    }
+
+    loadNewData(data) {
+        var loader = this.state.loaderData;
+        loader.isLoading = true;
+        data[loaderData] = loader;
+        this.setState(data, () => {
+            this.loadData(() => {
+                loader.isLoading = false;
+                this.setState({
+                    loadData: loader
+                })
+            })
+        });
+    }
+
+    async onPaginate(event,
+        data) {
+        let currentPage = data.activePage;
+        console.log("event data is............");
+        console.log(data.activePage);
+        console.log("current active page before setstate........");
+        console.log(this.state.activePage);
+        await this.setState(
+            {
+                activePage: currentPage
+            })
+        console.log("current active page is........");
+        console.log(this.state.activePage);
+        this.setState({ begin: this.state.activePage * 2 - 2 });
+        console.log("begin........");
+        console.log(this.state.begin);
+        this.setState({ end: this.state.activePage * 2 });
+        this.setState({
+                    currentJobs: this.state.loadJobs.slice(this.state.begin, this.state.end)
+                });
+        console.log("current jobs......onpagination");
+        console.log(this.state.currentJobs);
+    }
+
+    render() {
+        let JobList = this.state.currentJobs;
+        let JobDetails = null;
+        if (JobList != "") {
+            //let index = 0;
+            //JobDetails = JobList.map((job,index) => {
+            //    if (index >= this.state.begin && index <= this.state.end)                 
+            //    return( 
+            //        <Table.Row >
+            //        <Table.Cell key={job.id}>
+            //            <JobSummaryCard id={job.id} />
+            //        </Table.Cell>
+            //        </Table.Row >
+            //        )
+            //    index++;
+            //}
+            //);
+
+            JobDetails = JobList.map(job =>                  
+                    <Table.Row >
+                    <Table.Cell key={job.id}>
+                        <JobSummaryCard id={job.id} />
+                    </Table.Cell>
+                    </Table.Row >
+            );
+            
+        }
+        
+        return (
+            <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
+                <div className="ui container">
+                    <div className="sixteen wide left aligned padded column">
+                        <h1>Create Job</h1>
+                    </div>
+                    <Table>
+                        <Table.Body>
+                            {JobDetails} 
+                        </Table.Body>
+                    </Table> 
+                    <Pagination
+                        activePage={this.state.activePage}
+                        totalPages={Math.ceil(this.state.loadJobs.length / 2)}
+                        onPageChange={this.onPaginate}
+                         />
+                </div>
+            </BodyWrapper>
+            
+        )
+    }
+}
